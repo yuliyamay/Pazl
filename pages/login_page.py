a84f98bb0e54de0a53083e3685096f5dcdbc4237
Laysan Nova <laysansh@gmail.com>
2022-11-23 11:22:05 -0700

    items displayed

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,4 +1,3 @@
-
 from pages.base_page import BasePage
 from pages.locators import LoginPageLocators
 
---
+++ tests/test_item_is_displayed.py
@@ -0,0 +1,33 @@
+import time
+
+import pytest
+from pages.login_page import LoginPage
+from pages.locators import LoginPageLocators
+from pages.locators import InventoryPageLocators
+
+link = "https://www.saucedemo.com/"
+
+regular_user = "standard_user"
+password = "secret_sauce"
+
+
+@pytest.mark.TC003_02
+def test_footer_item_displayed(browser):
+    driver = LoginPage(browser, link)
+    driver.open_main_page()
+    driver.enter_user_name(regular_user)
+    driver.enter_user_password(password)
+    driver.click_login_button()
+    driver.should_be_current_page("https://www.saucedemo.com/inventory.html")
+    # footer_exist = driver.element_is_present(*InventoryPageLocators.FOOTER_SECTION)
+    # assert footer_exist, "footer section is not displayed ."
+    footer_robot_image_exist = driver.element_is_present(*InventoryPageLocators.TERMS_OF_SERVICE)
+    assert footer_robot_image_exist, "swag-bot image is not displayed ."
+
+
+@pytest.mark.TC001_13
+def test_username_displayed(browser):
+    driver = LoginPage(browser, link)
+    driver.open_main_page()
+    user_field_exist = driver.element_is_present(*LoginPageLocators.LOGIN_USER)
+    assert user_field_exist, "field username is not exist."
