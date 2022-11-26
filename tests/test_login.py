6d7bec4c2b78709fee682555b3e8822d45801d34
MarinaAvr <marinakhudoiarova@gmail.com>
2022-11-25 22:37:17 -0600

    Added more tests TC001_19, TC001_21_01, TC001_21_02, TC001_21_5

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,11 +1,14 @@
 import pytest
 from pages.login_page import LoginPage
+from selenium.webdriver.common.by import By
+
 link = "https://www.saucedemo.com/"
 
 regular_user = "standard_user"
 password = "secret_sauce"
 invalid_user = "invalid"
 invalid_password = "invalid"
+locked_out_user = "locked_out_user"
 
 
 @pytest.mark.smoke
@@ -52,3 +55,42 @@ def test_login_empty_fields(browser):
     assert (
         error_text == "Epic sadface: Username is required"
     ), "wrong warning text"
+
+
+@pytest.mark.TC001_19
+def test_hidden_password(browser):
+    driver = LoginPage(browser, link)
+    driver.open_main_page()
+    driver.enter_user_name(regular_user)
+    driver.enter_user_password(password)
+    # browser = webdriver.Chrome(ChromeDriverManager().install())
+    pas_locator = "//div/input[@type='password']"
+    assert browser.find_element(By.XPATH, pas_locator).get_attribute('type'), "password"
+
+@pytest.mark.TC001_21_01
+def test_user_is_redirected_to_inventory_page_standard_user(browser):
+    driver = LoginPage(browser, link)
+    driver.open_main_page()
+    driver.enter_user_name(regular_user)
+    driver.enter_user_password(password)
+    driver.click_login_button()
+    driver.should_be_current_page("https://www.saucedemo.com/inventory.html")
+
+@pytest.mark.xfail
+def test_user_is_redirected_to_inventory_page_locked_out_user(browser):
+    driver = LoginPage(browser, link)
+    driver.open_main_page()
+    driver.enter_user_name(locked_out_user)
+    driver.enter_user_password(password)
+    driver.click_login_button()
+    driver.should_be_current_page("https://www.saucedemo.com/inventory.html")
+
+@pytest.mark.TC001_21_05
+def test_login_locked_out_user(browser):
+    driver = LoginPage(browser, link)
+    driver.open_main_page()
+    driver.enter_user_name(locked_out_user)
+    driver.enter_user_password(password)
+    driver.click_login_button()
+    error_text = driver.getting_error_text()
+    assert error_text == 'Epic sadface: Sorry, this user has been locked out.', "wrong warning text"
