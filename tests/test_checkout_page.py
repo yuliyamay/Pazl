8dc9edc1ea55e5dfcb3aaac8ad927993ef40073c
yuliyamay <113870912+yuliyamay@users.noreply.github.com>
2022-12-11 10:52:44 -0500

    Yp (#37)
    * checkout tests
    
    * :art: Format Python code with psf/black
    
    Co-authored-by: yuliyamay <yuliyamay@users.noreply.github.com>

--- tests/test_checkout_page.py
+++ tests/test_checkout_page.py
@@ -1,11 +1,10 @@
 import time
 
 import pytest
-from pages.inventory_page import InventoryPage
 from pages.locators import InventoryPageLocators
 from pages.locators import CartPageLocators
 from pages.locators import CheckOutYourInformationPage
-
+from pages.inventory_page import InventoryPage
 
 link = "https://www.saucedemo.com/"
 
@@ -46,3 +45,44 @@ def test_error_first_name_is_required(browser):
     driver.click_element(*CheckOutYourInformationPage.CONTINUE_BUTTON)
     error_text = driver.error_first_name_is_required()
     assert error_text == "Error: First Name is required", "wrong warning text"
+
+
+@pytest.mark.TC_009_002
+def test_redirected_page(browser):
+    driver = InventoryPage(browser, link)
+    driver.login_success(browser)
+    driver.click_element(*InventoryPageLocators.BACKPACK_ADD_TO_CART_BUTTON)
+    driver.click_element(*InventoryPageLocators.SHOPPING_CART)
+    driver.click_element(*CartPageLocators.CHECKOUT_BUTTON)
+    driver.keyboard_input(*CheckOutYourInformationPage.CHECKOUT_FIRST_NAME, "John")
+    driver.keyboard_input(*CheckOutYourInformationPage.CHECKOUT_LAST_NAME, "Smith")
+    driver.keyboard_input(*CheckOutYourInformationPage.ZIP_CODE_FIELD, "61007")
+    driver.click_element(*CheckOutYourInformationPage.CONTINUE_BUTTON)
+    driver.should_be_current_page("https://www.saucedemo.com/checkout-step-two.html")
+
+
+@pytest.mark.TC_009_003
+def test_error_first_name(browser):
+    driver = InventoryPage(browser, link)
+    driver.login_success(browser)
+    driver.click_element(*InventoryPageLocators.BACKPACK_ADD_TO_CART_BUTTON)
+    driver.click_element(*InventoryPageLocators.SHOPPING_CART)
+    driver.click_element(*CartPageLocators.CHECKOUT_BUTTON)
+    driver.keyboard_input(*CheckOutYourInformationPage.CHECKOUT_FIRST_NAME, "John")
+    driver.click_element(*CheckOutYourInformationPage.CONTINUE_BUTTON)
+    error_text = driver.error_last_name_is_required()
+    assert error_text == "Error: Last Name is required", "wrong warning text"
+
+
+@pytest.mark.TC_009_05
+def test_postal_error(browser):
+    driver = InventoryPage(browser, link)
+    driver.login_success(browser)
+    driver.click_element(*InventoryPageLocators.BACKPACK_ADD_TO_CART_BUTTON)
+    driver.click_element(*InventoryPageLocators.SHOPPING_CART)
+    driver.click_element(*CartPageLocators.CHECKOUT_BUTTON)
+    driver.keyboard_input(*CheckOutYourInformationPage.CHECKOUT_FIRST_NAME, "John")
+    driver.keyboard_input(*CheckOutYourInformationPage.CHECKOUT_LAST_NAME, "Smith")
+    driver.click_element(*CheckOutYourInformationPage.CONTINUE_BUTTON)
+    error_text = driver.error_zipcode_required()
+    assert error_text == "Error: Postal Code is required", "wrong warning text"
